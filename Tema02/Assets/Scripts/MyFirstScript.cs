ce7018996c30427c10f32ba100b7b004d0498553
Tomeu de la Parte <tomeudelapartemulet@gmail.com>
2021-10-05 12:56:45 +0200

    Programacion 05/10/2021
    Variables básicas

---
+++ Tema02/Assets/Scripts/MyFirstScript.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyFirstScript : MonoBehaviour
+{
+
+    public int playerAge = 35;
+    private float playerSpeed = 5.25f;
+    public string playerName = "Tomeu";
+    [SerializeField] private bool gameOver;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
