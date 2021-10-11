0242b6cd3996ffa6ed60e237675c4a1e6703972e
Tomeu de la Parte <tomeudelapartemulet@gmail.com>
2021-10-11 10:41:45 +0200

    Updated.

--- Tema02/Assets/Scripts/MyFirstScript.cs
+++ Tema02/Assets/Scripts/MyFirstScript.cs
@@ -4,24 +4,38 @@ using UnityEngine;
 
 public class MyFirstScript : MonoBehaviour
 {
-
+    /*
     public int playerAge = 35;
     private float playerSpeed = 5.25f;
     public string playerName = "Tomeu";
     [SerializeField] private bool gameOver;
+    */
+
+    public int x = 5;
+    public int y = 2;
+
+    public float a = 5f;
+    public float b = 2f;
 
 
     // Start is called before the first frame update
     void Start()
     {
+        Debug.Log(x + y);
+        Debug.Log(x - y);
+        Debug.Log(x * y);
+        Debug.Log(x / y);
+        Debug.Log(x % y);
 
-
-        
+        Debug.Log($"Suma: {x} + {y} = {x + y}");
+        Debug.Log("Resta: " + x + " - " + y + " = " + (x - y));
+        Debug.Log($"Producto: {x} / {y} = {x / y}");
+        Debug.Log($"Resto: {x} % {y} = {x % y}");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
--- Tema02/Assets/Scripts/Vehicle.cs
+++ Tema02/Assets/Scripts/Vehicle.cs
@@ -4,11 +4,17 @@ using UnityEngine;
 
 public class Vehicle : MonoBehaviour
 {
+    // Variable que guarda el sonido del vehiculo
+    [SerializeField] private string sound = "¡Brum, brum!";
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+
+ 
+
+    } 
 
     // Update is called once per frame
     void Update()
